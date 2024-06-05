11c61e86f48f84c78f69e110633e5dc940c4135f
Firdi Audi Putra <39665978+bruteforce10@users.noreply.github.com>
2024-06-05 15:06:06 +0700

    add sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.parkspring.co.id",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+  ];
+}
