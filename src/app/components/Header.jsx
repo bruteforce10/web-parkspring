c7d70f223e8c0fe2d58f5389a014272b919abfe6
bruteforce10 <audifirdi@gmail.com>
2025-06-13 11:27:33 +0700

    style: style change parkspring button

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -32,17 +32,17 @@ const Header = () => {
                   "https://api.whatsapp.com/send?phone=%20628988187773&text=Hai,%20saya%20dapat%20info%20dari%20website%20PARKSPRING%20Gading"
                 )
               }
-              className="flex gap-4 max-lg:mx-auto text-white items-center w-fit text-lg border-gray-50 bg-white/5 border-2 backdrop-blur-xl shadow-sm lg:font-semibold before:absolute before:w-full relative z-10 px-8 py-3 overflow-hidden  rounded-full group max-sm:text-base max-sm:px-4 max-sm:py-2"
+              className="flex gap-4 max-lg:mx-auto text-white items-center w-fit text-lg bg-secondary border-[#d1bb40] shadow-sm lg:font-semibold before:absolute before:w-full relative z-10 px-8 py-3 overflow-hidden  rounded-full group max-sm:text-base max-sm:px-4 max-sm:py-2 border-2"
             >
               Dapatkan Promonya Sekarang
               <svg
-                className="w-8 h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full border border-gray-50 group-hover:border-none p-2 rotate-45"
+                className="w-8 h-8 justify-end group-hover:rotate-90 group-hover:bg-white/50 backdrop-blur-lg text-gray-50 ease-linear duration-300 rounded-full border border-gray-200 group-hover:border-2 p-2 rotate-45"
                 viewBox="0 0 16 19"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
                   d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z"
-                  className="fill-gray-50 group-hover:fill-gray-800/30"
+                  className="fill-gray-200 group-hover:fill-gray-100"
                 ></path>
               </svg>
             </button>
