3c419e38e4731056864688fbd244e6800b81ce16
bruteforce10 <audifirdi@gmail.com>
2025-06-24 09:56:26 +0700

    feat & fix: implement Google Tag Manager integration with updated script handling

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,8 +4,7 @@ import Navbar from "./components/Navbar";
 import { ContextProvider } from "./utils/stateContext";
 import CallToAction from "./components/CallToAction";
 import { Analytics } from "@vercel/analytics/react";
-import GoogleTagManagerScript from "@/lib/Head";
-import GoogleTagManagerNoScript from "@/lib/Body";
+import Script from "next/script";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
@@ -23,14 +22,35 @@ export const metadata = {
   },
 };
 
+const GTM_ID = "GTM-KHCKPCVT";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* GTM Script */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+      </head>
       <Analytics />
-      <GoogleTagManagerScript />
-      <GoogleTagManagerNoScript />
       <ContextProvider>
         <body className={outfit.className}>
+          {/* GTM noscript */}
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
           <div className=" top-0 z-[99] sticky">
             <Navbar />
           </div>
