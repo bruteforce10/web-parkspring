3878afeac01372f8fe1721a85af8ad9aa0bd40e3
bruteforce10 <audifirdi@gmail.com>
2025-12-19 13:54:54 +0700

    fix: update openGraph title for consistency with property description

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ export const metadata = {
   // kalo beda pakai ini
   openGraph: {
     images: "/opengraph-image.png",
-    title: "PARKSPRING Gading, Rumah 1,5 M-an Japanese Tropical",
+    title: "PARKSPRING Gading, Rumah 2M Bergaya Japanese Tropical",
     description:
       "Fasilitas PARKSPRING Gading · One Gate System & 24 Hours Security with Security Hub · Zen Garden · Fish Feeding Pond · Hidden Yoga Spot · Children Playground.",
   },
