2563e440eb92571dc1c3d47f8f56d69f6f2d6ee2
bruteforce10 <audifirdi@gmail.com>
2025-12-19 11:29:35 +0700

    fix: update title in metadata for consistency with property description

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,7 +9,7 @@ const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
   metadataBase: new URL("https://web-parkspring.vercel.app"),
-  title: "PARKSPRING Gading, Rumah 1,5 M-an Japanese Tropical",
+  title: "PARKSPRING Gading, Rumah 2M Bergaya Japanese Tropical",
   description:
     "Fasilitas PARKSPRING Gading · One Gate System & 24 Hours Security with Security Hub · Zen Garden · Fish Feeding Pond · Hidden Yoga Spot · Children Playground.",
   // kalo beda pakai ini
